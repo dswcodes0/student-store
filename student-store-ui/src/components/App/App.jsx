fb19722770c0caf22c62700f0b93a3f37605c7fe
Darren Washington <dsw@meta.com>
2023-06-23 14:21:58 -0700

    added font awesome and navbar component

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import Home from "../Home/Home";
@@ -8,13 +8,13 @@ import "./App.css";
 
 export default function App() {
   return (
-    
     <div className="app">
       <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/product/:productId" element={<ProductDetail />} />
-      </Routes>
+        <Navbar /> 
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,37 @@
-import * as React from "react"
-import "./Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      {/* <div className="content">
-        <div className="logo">
-          <a href="">
-            <img className="kimetsu-logo" src="kimetsu-logo.png" alt="" />
-          </a>
-        </div>
-        <div className="socials"></div>
-        <div className="links"></div>
-      </div> */}
+      <div className="navbar-logo">
+        <img src="/kimetsu-logo.png" alt="Logo" />
+      </div>
+      <div className="navbar-social">
+        <a href="https://example.com">
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a href="https://example.com">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://example.com">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+        <Link to="/about" className="navbar-link">
+          About
+        </Link>
+        <Link to="/contact" className="navbar-link">
+          Contact
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
